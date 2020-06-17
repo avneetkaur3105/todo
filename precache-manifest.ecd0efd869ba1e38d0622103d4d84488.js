30850e7e27493b64dc4bbad25e17a5eb7be286fd
avneet kaur <avneetkaur3105@gmail.com>
2020-06-17 11:47:31 +0530

    Updates

---
+++ precache-manifest.ecd0efd869ba1e38d0622103d4d84488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfe05ea467ae7d97b5e2c644ec4e771",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "6917d0a12141a10d1441",
+    "url": "/todo/static/css/main.003ba3ae.chunk.css"
+  },
+  {
+    "revision": "779dcde8dd36bd2ad828",
+    "url": "/todo/static/js/2.915ef1b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.915ef1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6917d0a12141a10d1441",
+    "url": "/todo/static/js/main.0ad956a1.chunk.js"
+  },
+  {
+    "revision": "0f6af5e99157b16edf1b",
+    "url": "/todo/static/js/runtime-main.5f58cc0b.js"
+  }
+]);
